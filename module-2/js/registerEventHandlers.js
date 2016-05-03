baae77a771db0f72bfe1365df836a9bb747827a8
bgrove1974 <bgroveben@gmail.com>
2016-05-03 02:03:52 -0700

    Fix errors in HTML and JS files for event handler demo

--- module-2/js/registerEventHandlers.js
+++ module-2/js/registerEventHandlers.js
@@ -1,5 +1,5 @@
 $(function() {
-  // display the IDs and classes on buttons
+  // display the ids and classes on buttons
   updateButtonDisplay();
 
   // execute the code inside the pre element when clicked
@@ -9,20 +9,21 @@ $(function() {
     // eval will run a string as JavaScript
     eval(codeBlock.text());
 
-    // update the display
+    // Update the display
     codeBlock.append('<div class="text-center"><strong>code executed</strong></div>');
     codeBlock.removeClass('code').addClass('code-executed');
+
 
     updateButtonDisplay();
   });
 });
 
 function appendDisplay() {
-  // get a jQuery wrapper for the clicked button
-  var button =$(this);
-  // find the item with the display class
+  // Get a jQuery wrapper for the clicked button
+  var button = $(this);
+  // Find the item with the display class
   var display = button.nextAll('.display');
-  // add a new list item with text clicked
+  // Add a new list item with text Clicked
   display.append('<li>Clicked</li>');
 }
 
@@ -31,10 +32,12 @@ function updateButtonDisplay() {
     var button = $(this);
     button.empty();
 
-    // if the is an ID, display it
-    if(this.id.length > 0) button.append("#" + this.id);
+    // if there's an ID, display it
+    if(this.id.length > 0) button.append('#' + this.id);
+
     // retrieve all classes
     var classes = button.attr('class');
+
     // remove any Bootstrap classes
     $.each(classes.split(' '), function() {
       if(this.indexOf('btn') == -1) button.append('<div>.' + this + '</div>')
