53dba871a82178e371fa2d877748b14a541b2fca
bgrove1974 <bgroveben@gmail.com>
2016-05-04 18:07:04 -0700

    Finish JS for interactive demo -- jQuery DOM manipulations

--- module-2/js/addingElements.js
+++ module-2/js/addingElements.js
@@ -1,3 +1,56 @@
+// Begin HTML display utilities file:
+var $startingHtml = null;
+$(function() {
+  // Grab a copy of the initial state.
+  $startingHtml = $('#html-content').clone();
+  displayHtml();
+});
+
+function resetHtml() {
+  // Replace the current content with a copy of the initial state
+  $('#html-content').replaceWith($startingHtml.clone());
+}
+
+function displayHtml(data) {
+  // default to the entire group
+  if (data === undefined) data = '#html-content';
+
+  // loop through all data
+  $.each($(data).children(), function() {
+    createHtml(this);
+  });
+}
+
+function createHtml(element) {
+  // ensure it's a jQuery object
+  element = $(element);
+
+  // if there are children, recurse
+  if (element.children().length > 0) {
+    displayHtml(element);
+  }
+
+  var startingTag = element.prop('outerHTML').substring(0, element.prop('outerHTML').indexOf('>') + 1);
+  var tagName = '';
+  if (startingTag.indexOf(' ') > -1)
+    tagName = startingTag.substring(1, startingTag.indexOf(' '));
+  else
+    tagName = startingTag.substring(1, startingTag.indexOf('>'));
+  var formattedTag = '&lt;<span class="tag">' + tagName + '</span>attributes&gt;';
+
+  var attributesString = '';
+  $.each(element.prop('attributes'), function() {
+    attributesString += ' ' + this.nodeName + '=';
+    attributesString += '<span class="attribute-value">"' + this.nodeValue + '"</span>';
+  });
+
+  startingTag = formattedTag.replace('attributes', attributesString);
+  element.prepend(startingTag);
+  element.append(formattedTag.replace('attributes', '').replace('&lt;', '&lt;/'));
+
+  element.css('padding-left', '20px');
+}
+
 $(function() {
   $('#new-element').val('<div id="demo"></div');
   $('#target-id').val('starter');
@@ -25,3 +78,20 @@ $(function() {
     $('#output').text($(this).text());
   });
 });
+
+function updateButtons() {
+  var newElement = $('#new-element').val().replace("'", '"');
+  var targetId = $('#target-id').val();
+
+  $('#btn-before').text("$('#" + targetId + "').before('" + newElement + "');");
+  $('#btn-insertBefore').text("$('" + newElement + "').insertBefore('#" + targetId + "');");
+
+  $('#btn-after').text("$('#" + targetId + "').after('" + newElement + "');");
+  $('#btn-insertAfter').text("$('" + newElement + "').insertAfter('#" + targetId + "');");
+
+  $('#btn-prepend').text("$('#" + targetId + "').prepend('" + newElement + "');");
+  $('#btn-prependTo').text("$('" + newElement + "').prependTo('#" + targetId + "');");
+
+  $('#btn-append').text("$('#" + targetId + "').append('" + newElement + "');");
+  $('#btn-appendTo').text("$('" + newElement + "').appendTo('#" + targetId + "');");
+}
