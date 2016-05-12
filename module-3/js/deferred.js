ff8db04c7a4a5ee83a5f154bd4bd3eb28d97f3c1
bgrove1974 <bgroveben@gmail.com>
2016-05-11 19:35:26 -0700

    Create style and structure for deferred example

--- module-3/js/deferred.js
+++ module-3/js/deferred.js
@@ -12,6 +12,6 @@ self.postMessage('COMPLETED');
 function sleep(milliseconds) {
   var startingTime = new Date().getTime();
   var stopTime = startingTime + milliseconds;
-
+  // As long as stopTime is greater than the current time, wait patiently and let calling script do its thing
   while (stopTime >= new Date().getTime()) { }
 }
